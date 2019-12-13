f1f03320a18356057b64892ff25d06044d1374d2
Harnas_20 <Harnas_20@o2.pl>
2019-12-13 14:56:28 +0100

    first and last commit he he

---
+++ AplikacjaSQL.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace SQLtest
+{
+    partial class AplikacjaSQL
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SQLview = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.SQLview)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // SQLview
+            // 
+            this.SQLview.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.SQLview.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.SQLview.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.SQLview.Location = new System.Drawing.Point(178, 74);
+            this.SQLview.MaximumSize = new System.Drawing.Size(850, 850);
+            this.SQLview.Name = "SQLview";
+            this.SQLview.Size = new System.Drawing.Size(850, 850);
+            this.SQLview.TabIndex = 0;
+            this.SQLview.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.SQLview_CellContentClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(150, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "procedura1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(507, 31);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "procedura2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(846, 31);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "procedura3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // AplikacjaSQL
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(1184, 913);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.SQLview);
+            this.Name = "AplikacjaSQL";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Aplikacja SQL ";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.SQLview)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView SQLview;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ AplikacjaSQL.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace SQLtest
+{
+    public partial class AplikacjaSQL : Form
+    {
+
+        string conectionString = "Data Source=KZ-119GOLABEK2\\SQLEXPRESS;Initial Catalog = Test; Integrated Security = True";
+
+
+
+
+        public List<Person> People { get; set; }
+        public AplikacjaSQL()
+        {
+            People = GetPeople();
+            InitializeComponent();
+
+        }
+
+
+        private List<Person> GetPeople()
+        {
+           var list = new List<Person>();
+
+                list.Add(new Person(){
+                    name="Andrzej", 
+                    surname="Gołąbek",
+                    city="Krosno"
+               });
+
+                list.Add(new Person(){
+                name = "Andrzej",
+                surname = "Gołąbek",
+                city = "Krosno"
+            });
+            list.Add(new Person()
+            {
+                name = "Andrzej",
+                surname = "Gołąbek",
+                city = "Krosno"
+            });
+            list.Add(new Person()
+            {
+                name = "Andrzej",
+                surname = "Gołąbek",
+                city = "Krosno"
+            });
+
+            return list;
+        }
+
+
+        public void button3_Click(object sender, EventArgs e)
+        {
+
+
+            using (SqlConnection con = new SqlConnection(conectionString))
+            {
+                con.Open();
+
+                if (con.State == System.Data.ConnectionState.Open)
+                {
+                    string query = "EXEC dbo.insertUsers @name='Roman', @surname='Buba', @city='Kraków'  ";
+                    // SqlCommand cmd = new SqlCommand(query, con);
+                    SqlDataAdapter sglDa = new SqlDataAdapter(query, con);
+                    DataTable dtbl = new DataTable();
+                    sglDa.Fill(dtbl);
+
+                    SQLview.DataSource = dtbl;
+
+
+
+                }
+            }
+
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+
+            var people = this.People;
+
+            SQLview.DataSource = people;
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            using (SqlConnection con = new SqlConnection(conectionString)) {
+                con.Open();
+
+                if (con.State == System.Data.ConnectionState.Open)
+                {
+                    string query = "Exec dbo.showusers";
+                    // SqlCommand cmd = new SqlCommand(query, con);
+                    SqlDataAdapter sglDa = new SqlDataAdapter(query,con);
+                    DataTable dtbl = new DataTable();
+                    sglDa.Fill(dtbl);
+
+                    SQLview.DataSource = dtbl;
+
+
+
+            }
+            }
+
+        }
+
+        private void SQLview_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            MessageBox.Show("Wywołana procedura");
+        }
+    }
+}
